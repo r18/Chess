79f2f0cd76c1620aec7f4a570f76a7767ccb82c4
r18 <hrl.7th@gmail.com>
2014-03-27 01:59:34 +0900

    Merged

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 <html lang="en">
 <head>
  <meta charset="UTF-8">
+  <script src="js/main.js"></script>
+  <script src="lib/three.js/build/three.js"></script>
  <title></title>
 </head>
-<body>
+<body onload="main()">
   <h1>Chess Game!!</h1>
+  <div id="cvs"></div>
 </body>
 </html>
---
+++ js/main.js
@@ -0,0 +1,84 @@
+function main(){
+  mesh="";
+  camera ="";
+  scene = "";
+  theta = 0;
+  init();
+  animate();
+  window.onkeydown = function(e){
+    console.log(e.keyCode); 
+   switch(e.keyCode){
+      case 38:
+        camera.position.y += 0.5;
+  camera.lookAt(mesh.position);
+        draw();
+        break;
+      case 37:
+        camera.position.x -= 0.5;
+  camera.lookAt(mesh.position);
+        draw();
+        break;
+      case 40:
+        camera.position.y -= 0.5;
+  camera.lookAt(mesh.position);
+        draw();
+        break;
+      case 39:
+        camera.position.x += 0.5;
+  camera.lookAt(mesh.position);
+        draw();
+        break;
+      default:
+       break;
+
+    }
+
+  }
+}
+
+function init(){
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75,300 / 300,0.1,1000);
+
+  camera.position.z = 17;
+  camera.position.y = 8;
+  scene.add(camera);
+
+  light = new THREE.DirectionalLight(0xffffff,10);
+  light.position.set(0, 1, 0).normalize();
+  scene.add(light);
+
+  cvs = document.getElementById("cvs");
+  renderer = new THREE.WebGLRenderer( {antialias: true} );
+  renderer.setSize(600,600);
+  var loader = new THREE.JSONLoader();
+  loader.load("js/queen.js",createScene);
+
+  cvs.appendChild(renderer.domElement);
+
+}
+
+function createScene( geometry ) {
+ 	var material = new THREE.MeshLambertMaterial( { color: 0x660000 } );
+  mesh = new THREE.Mesh( geometry, material );
+  scene.add( mesh );
+  render();
+}
+
+function animate(){
+  requestAnimationFrame( animate );
+  render();
+}
+
+function render() {
+  camera.position.x = Math.sin(theta)*7; 
+  camera.position.z = Math.cos(theta)*7 ;
+  camera.position.y = Math.cos(theta/3)*7+4;
+  theta += 0.05;
+  
+  camera.lookAt(mesh.position)+
+  draw();
+}
+function draw(){
+  renderer.render(scene,camera);
+}
